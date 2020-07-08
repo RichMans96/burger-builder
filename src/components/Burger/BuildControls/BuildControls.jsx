e47cb8d17b3b395f1208aa1ae2834f7a4a37db16
Richard <richard.mansworth@hotmail.co.uk>
2020-07-08 17:15:54 +0100

    user can now add and remove ingredients

---
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./BuildControls";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Meat", type: "meat" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Bacon", type: "bacon" },
+];
+
+const BuildControls = (props) => {
+  return (
+    <div className="BuildControls">
+      {controls.map((control) => (
+        <BuildControl
+          key={control.label}
+          label={control.label}
+          added={() => props.ingredientAdded(control.type)}
+          removed={() => props.ingredientRemoved(control.type)}
+          disabled={props.disabled[control.type]}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BuildControls;
