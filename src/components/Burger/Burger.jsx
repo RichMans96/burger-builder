17d43f7ba5b4ffbe3fcec6b15c8fd7b54b84df45
Richard <richard.mansworth@hotmail.co.uk>
2020-07-08 16:31:47 +0100

    If no ingredients are added, user is notified

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,12 +4,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import "./Burger.css";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, index) => {
-      return <BurgerIngredient key={igKey + index} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add ingredients</p>;
+  }
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
